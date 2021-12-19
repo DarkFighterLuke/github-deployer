5f5b8583ec20714bda8b55c53c1fa41124573fc7
DarkFighterLuke <luca-consoli@live.it>
2021-12-20 00:40:09 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"githubDeployer/utils"
+	"io"
+	"log"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+var configPath = "conf.yml"
+var conf *utils.Config
+
+func main() {
+	var err error
+	conf, err = utils.GetConfig(configPath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/payload", payloadHandler).Methods("POST")
+	err = http.ListenAndServe(conf.Server.Host+":"+conf.Server.Port, r)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func payloadHandler(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	temp := strings.Split(string(body), ",")
+	temp = strings.Split(temp[0], ":")
+	branch := strings.Split(strings.Replace(temp[1], "\"", "", -1), "/")[2]
+	log.Println("Received event on branch " + branch)
+	if branch == conf.Repository.Branch {
+		log.Println("Running " + conf.Script.Path + "...")
+
+		err := exec.Command("chmod", "+x", conf.Script.Path).Run()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		err = exec.Command("/bin/bash", conf.Script.Path).Run()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println("Execution of " + conf.Script.Path + " finished.")
+	}
+}
---
+++ utils/configuration.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port string `yaml:"port"`
+	} `yaml:"server"`
+	Repository struct {
+		Branch string `yaml:"branch"`
+	} `yaml:"repository"`
+	Script struct {
+		Path string `yaml:"path"`
+	} `yaml:"script"`
+}
+
+//GetConfig Given a configuration path validate and return new Config
+func GetConfig(configPath string) (*Config, error) {
+	err := ValidateConfigPath(configPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	conf, err := NewConfig(configPath)
+	return conf, err
+}
+
+//NewConfig Given a configuration path read a new Config
+func NewConfig(congigPath string) (*Config, error) {
+	config := &Config{}
+
+	file, err := os.Open(congigPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	d := yaml.NewDecoder(file)
+
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+//ValidateConfigPath Check if path is valid or not
+func ValidateConfigPath(path string) error {
+	s, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if s.IsDir() {
+		return fmt.Errorf("'%s' is a directory, not a normal file", path)
+	}
+	return nil
+}
